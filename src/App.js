38457f50595c365cc6459810bc5c84ec333fe206
Juan Basualdo <basualdo.ij@gmail.com>
2022-07-14 01:58:39 -0300

    Implemented the logic to display popup modal if any error occurs with user inputs both in 'username' input and/or in 'age' input.

--- src/App.js
+++ src/App.js
@@ -5,38 +5,51 @@ import Container from './components/UI/Container'
 import UserList from './components/User/UserList';
 
 const App = () => {
-  const [isPopUp, setIsPopUp] = useState(false);
+  const USERNAME_ERROR_TITLE = "Invalid Username";
+  const USERNAME_ERROR_MESSAGE = "Please enter one valid username.";
+  const AGE_ERROR_TITLE = "Invalid input age";
+  const AGE_ERROR_MESSAGE = "Age can't be a negative number.";
+  
+  const [isPopUp, setIsPopUp] = useState('');
   const [usersList, setUsersList] = useState([]);
   const [ errorMessage, setErrorMessage ] = useState("Error message.");
   const [ errorTitle, setErrorTitle ] = useState("Error title");
 
-  const validateAgeInput = (user) =>{
-    let valid = true;
-
-    if(user.age < 0){
-      valid = false; 
-      setErrorMessage("Age can't be negative.");
-      setErrorTitle("Invalid input age")
+  const displayUsernameErrorModal = () => {
+      setErrorTitle(USERNAME_ERROR_TITLE)
+      setErrorMessage(USERNAME_ERROR_MESSAGE);
       setIsPopUp(true);
-    }
-    
-    return valid;
   }
 
-  const addUserToList = (user) => {
+  const displayAgeErrorModal = () =>{
+      setErrorTitle(AGE_ERROR_TITLE)
+      setErrorMessage(AGE_ERROR_MESSAGE);
+      setIsPopUp(true);
+  }
+
+  const displayErrorModal = (input) => {
     debugger
-    if(!validateAgeInput(user)) return;
-
-    setUsersList( prevUserList => [ user , ...prevUserList ]);
+    switch (input) {
+      case 'username':
+        displayUsernameErrorModal();
+        break;
+      case 'age':
+        displayAgeErrorModal();
+        break;
+      default:
+        break;
+    }
   }
+
+  const addUserToList = (user) => setUsersList( prevUserList => [ user , ...prevUserList ]);
 
   const closePopup = () => setIsPopUp(false);
-  
+
   return (
     <>
-      {!isPopUp && <PopUp errorMessage={errorMessage} errorTitle={errorTitle} closePopup={closePopup} />}
+      {isPopUp && <PopUp errorMessage={errorMessage} errorTitle={errorTitle} closePopup={closePopup} />}
     <Container>
-      <UserInputForm addUser={addUserToList}/>
+      <UserInputForm addUser={addUserToList} displayErrorModal={displayErrorModal}/>
       <UserList userList={usersList}/>
     </Container>
     </>
