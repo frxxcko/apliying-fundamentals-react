2c1f58d9e7d168b4b2b33c1cb059b4766fc738a2
Juan Basualdo <basualdo.ij@gmail.com>
2022-07-14 01:55:41 -0300

    Add tabindex to list items so Screen Readers can show the user that it's an interactive element

--- src/components/User/UserItem.js
+++ src/components/User/UserItem.js
@@ -2,18 +2,15 @@ import React from 'react'
 import styles from './UserItem.module.css'
 
 const UserItem = (props) => {
-    const firstNameWithFirstLetterToUpperCase = props.username.at(0).toUpperCase() + props.username.slice(1).split(" ")[0]??"";
-    const secondName = props.username.split(" ")[1] ?? '';
-    debugger
-    const secondNameFirstLetterToUpperCase = secondName?.at(0)?.toUpperCase() + secondName?.slice(1) ?? '';
-    
+    const firstName = props?.username?.at(0);
+    const firstNameWithFirstLetterToUpperCase = !firstName? "" : firstName.toUpperCase() + props?.username?.slice(1);
 
     const clickItemHandler = (e) => {
         console.log(e);
     }
     
     return (
-    <li className={styles.item} onClick={clickItemHandler}>
+    <li className={styles.item} onClick={clickItemHandler} tabIndex='0'>
         <h3>{`${firstNameWithFirstLetterToUpperCase}`}</h3>
         <h4>Age: {props.age}</h4>
     </li>
