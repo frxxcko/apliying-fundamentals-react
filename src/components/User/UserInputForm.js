b3368c9045ea69c20133d37fa1d6073ba497d69e
Juan Basualdo <basualdo.ij@gmail.com>
2022-07-14 01:57:34 -0300

    Add validations for user inputs, both in 'username' and 'age'.

--- src/components/User/UserInputForm.js
+++ src/components/User/UserInputForm.js
@@ -4,24 +4,38 @@ import Card from '../UI/Card'
 import styles from './UserInputForm.module.css'
 
 const UserInputForm = (props) => {
-    
     const [username, setUsername] = useState('');
     const [userAge, setUserAge] = useState(0);
 
     const captureUsername = (e) => setUsername(e.target.value);
     const captureUserAge = (e) => setUserAge(e.target.value);
 
+    const isUserInputValid = (username, age) => {
+        let isValidInput = true;
+        debugger
+        if(!username || username.split(' ').length > 1)
+        {
+            isValidInput = false;
+            props.displayErrorModal('username');
+        }
+        else if(age < 0)
+        {
+            isValidInput = false;
+            props.displayErrorModal('age');
+        }
+        
+        return isValidInput;
+    }
+
     const submitHandler = (event) => {
         event.preventDefault();
         debugger
-
+        if(!isUserInputValid(username, userAge)) return;
 
         const user = {
-            username: username,
+            username: username.trim(),
             age: userAge
         }
-
-
         props.addUser(user)
     }
 
@@ -29,10 +43,10 @@ const UserInputForm = (props) => {
         <Card >
             <form onSubmit={submitHandler} className={styles.form_control}>
                 <label htmlFor="username">Username</label>
-                <input required onChange={captureUsername} type="text" id="username" placeholder='Please type an username' autoComplete='off'/>
+                <input onChange={captureUsername} type="text" id="username" placeholder='Please type an username' autoComplete='off'/>
                 
                 <label htmlFor="age">Age (Years)</label>
-                <input required onChange={captureUserAge} type="number" id="age" placeholder='Please type your age' />
+                <input onChange={captureUserAge} type="number" id="age" placeholder='Please type your age' />
 
                 <Button type='submit'>Submit</Button>
             </form>
