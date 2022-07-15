04da5e3ddb1545f3112f9acc9fd68747f106e256
Juan Basualdo <basualdo.ij@gmail.com>
2022-07-15 01:17:17 -0300

    Added a validation in mapping of usersList prop to not render the initial usersList state value. Also added a span to display error message of Max users reached on the users list. Last, added props.id to each 'li' item to be able to delete it by ID with deleteUserItem prop

--- src/components/User/UserList.js
+++ src/components/User/UserList.js
@@ -6,9 +6,9 @@ import styles from './UserList.module.css'
 const UserList = (props) => {
   return (
     <Card className={styles.card_list}>
-        <h2>List of users</h2>
+        <h2>List of users {props.showMaxUsersMessage && <span className={styles.span_message}>(max users reached)</span>}</h2>
         <ul className={styles.user_list}>
-            {props.userList.map( (user, index) => <UserItem username={user.username} age={user.age} key={`${user.username.replace(" ", "-")}-${index}`}/> )}
+            {props.userList.map( (user, index) => user.id !== 0 && <UserItem username={user.username} age={user.age} key={`${user.username.replace(" ", "-")}-${index}`} id={user.id} deleteUserItem={props.deleteUserItem}/> )}
         </ul>
     </Card>
   )
