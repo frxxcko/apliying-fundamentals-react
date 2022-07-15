1fd0d861378b481196e2d068e7f683eebc8f9eab
Juan Basualdo <basualdo.ij@gmail.com>
2022-07-15 01:12:37 -0300

    Added Accessibility attribute 'tabindex' to let the users interact with each UserItem and delete it by interacting with Space key if wanted. Also added a onClick event to execute props.deleteUserItem() method created in App.js component to delete item from the list of users

--- src/components/User/UserItem.js
+++ src/components/User/UserItem.js
@@ -5,12 +5,10 @@ const UserItem = (props) => {
     const firstName = props?.username?.at(0);
     const firstNameWithFirstLetterToUpperCase = !firstName? "" : firstName.toUpperCase() + props?.username?.slice(1);
 
-    const clickItemHandler = (e) => {
-        console.log(e);
-    }
+    const clickHandler = () => props.deleteUserItem(props.id);
     
     return (
-    <li className={styles.item} onClick={clickItemHandler} tabIndex='0'>
+    <li className={styles.item} tabIndex='0' onClick={clickHandler} id={props.id} onKeyDown={e => e.code === "Space" && clickHandler()}>
         <h3>{`${firstNameWithFirstLetterToUpperCase}`}</h3>
         <h4>Age: {props.age}</h4>
     </li>
