c67ffe3fd6cb0497acca679d63743d7eaf8739b1
Juan Basualdo <basualdo.ij@gmail.com>
2022-07-15 01:26:56 -0300

    Delete unnecessary identation

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,9 +2,6 @@ import React from 'react'
 import styles from './Button.module.css'
 
 const Button = (props) => {
-
-  
-
   return (
     <button className={styles.button} type={props.type} onClick={() => props.closePopup()}>
       {props.children}
