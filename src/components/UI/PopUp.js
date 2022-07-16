06202725155076d66accf1fd74c6d9c052f621a5
Juan Basualdo <basualdo.ij@gmail.com>
2022-07-16 15:59:48 -0300

    Working with Portals from react-dom

--- src/components/UI/PopUp.js
+++ src/components/UI/PopUp.js
@@ -1,23 +1,32 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import styles from './PopUp.module.css'
 import Card from './Card'
 import Button from './Button'
 import PopUpBackground from './PopUpBackground'
+
+const ErrorPopup = (props) => {
+  return (
+    <PopUpBackground closePopup={props.closePopup}>
+      <Card className={styles.card_popup}>
+        <div className={styles.container} role='alert'>
+          <h2 className={styles.title_header}>{props.errorTitle}</h2>
+          <p className={styles.p}>{props.errorMessage}</p>
+          <Button type='button' closePopup={props.closePopup}>Okay</Button>
+        </div>
+      </Card>
+    </PopUpBackground>
+  )
+}
 
 const PopUp = (props) => {
 
   const closePopup = () => props.closePopup();
 
   return (
-    <PopUpBackground closePopup={closePopup}>
-      <Card className={styles.card_popup}>
-        <div className={styles.container} role='alert'>
-          <h2 className={styles.title_header}>{props.errorTitle}</h2>
-          <p className={styles.p}>{props.errorMessage}</p>
-          <Button type='button' closePopup={closePopup}>Okay</Button>
-        </div>
-      </Card>
-    </PopUpBackground>
+    <>
+    {ReactDOM.createPortal(<ErrorPopup errorMessage={props.errorMessage} errorTitle={props.errorTitle} closePopup={closePopup}/>, document.getElementById('error-popup'))}
+    </>
   )
 }
 
